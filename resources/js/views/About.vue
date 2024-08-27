847c6fc2b9725aa76dbb21345f82615e7aa99329
Mahmudul Hasan <hasanmahmudul356@gmail.com>
2024-08-27 18:36:20 +0600

    Just Laravel and vue Configure

---
+++ resources/js/views/About.vue
@@ -0,0 +1,20 @@
+<script>
+export default {
+    name: "About.vue"
+}
+</script>
+
+<template>
+<div>
+    <h1>I am from About Component</h1>
+    <a href="/admin/home"><h1>Home</h1></a>
+    <a href="/admin/about"><h1>Abount</h1></a>
+
+    <router-link to="/admin/home"><h1>Home</h1></router-link>
+    <router-link to="/admin/about"><h1>Abount</h1></router-link>
+</div>
+</template>
+
+<style scoped>
+
+</style>
---
+++ resources/js/views/Home.vue
@@ -0,0 +1,21 @@
+<template>
+   <div>
+       <h1>I am from Home Component</h1>
+       <a href="/admin/home"><h1>Home</h1></a>
+       <a href="/admin/about"><h1>Abount</h1></a>
+
+       <router-link to="/admin/home"><h1>Home</h1></router-link>
+       <router-link to="/admin/about"><h1>Abount</h1></router-link>
+   </div>
+</template>
+
+<script>
+export default {
+    name: "Home",
+    mounted() {}
+}
+</script>
+
+<style scoped>
+
+</style>
